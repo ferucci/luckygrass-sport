7d5e6f7e41aa71cacede19e2a1a58d415c132a6a
Ferucci <development.first1@gmail.com>
2025-11-13 21:57:44 +0300

    Described by comment

--- src/data/shared/breadcrumbs.ts
+++ src/data/shared/breadcrumbs.ts
@@ -4,19 +4,14 @@ export interface BreadcrumbItem {
   active: boolean;
 }
 
+// Динамические данные хлебных крошек для страниц
 export const breadcrumbs = {
-  // Для главной страницы
-  home: [
-    { name: "Главная", url: "/", active: true }
-  ] as BreadcrumbItem[],
 
-  // Для страницы блога
   blog: [
     { name: "Главная", url: "/", active: false },
     { name: "Блог", url: "", active: true }
   ] as BreadcrumbItem[],
 
-  // Для страницы статьи
   article: [
     { name: "Главная", url: "/", active: false },
     { name: "Блог", url: "/blog", active: false },
